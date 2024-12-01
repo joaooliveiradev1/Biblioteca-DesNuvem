558a3907460ca06d6648c09fa94c0225400e026d
Pedro Lucas <pedrolucassaraiva_@outlook.com>
2024-12-01 16:20:13 -0300

    codigo refeito para solucionar os bugs

--- scripts/loginUser.js
+++ scripts/loginUser.js
@@ -3,6 +3,32 @@ const form = document.querySelector('form');
 
 
 
+btnEntrar.addEventListener('click', e => {
+    e.preventDefault();
+    const inputLogin = document.querySelector('#loginEntrada');
+    const passwordEntrada = document.querySelector('#passwordEntrada');
+
+    const usuario = {
+        "username": inputLogin.value.trim(),
+        "password": passwordEntrada.value.trim(),
+    };
+
+    fetch(`${window.location.origin}/login`, {
+        method: 'POST', 
+        headers: {'Content-Type': 'application/json'},  // ta informando que o conteudo é pelo JSON
+        body: JSON.stringify(usuario) // Convertendo objeto para String
+      })
+        .then(response => response.status)  // Se a resposta seja bem-sucedida, será realizado o login
+            .then(statusCode => {
+            if (statusCode == '200') {
+                window.location.href = './pages/library.html';
+            }else if ((statusCode == "401") || (statusCode == "500")) { // Caso não, vai informar que o usuario não foi encontrado
+                alert('Usuário não encontrado!');
+                passwordEntrada.value = '';
+                inputLogin.focus();
+            }
+        });
+});
 
 
 form.addEventListener('submit', e => {
